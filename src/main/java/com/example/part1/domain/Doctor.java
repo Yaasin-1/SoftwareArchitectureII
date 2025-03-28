0716e4e59175367a0c062fafe66c28aeecd4f122
Yaasin-1 <uddinyaasin@gmail.com>
2025-03-28 16:12:19 +0000

    Added attributes to Doctor class

--- src/main/java/com/example/part1/domain/Doctor.java
+++ src/main/java/com/example/part1/domain/Doctor.java
@@ -1,4 +1,9 @@
 package com.example.part1.domain;
 
 public class Doctor {
+    private Long id;
+    private String name;
+    private String specialisation;
+    private String email;
+    private String phoneNumber;
 }
